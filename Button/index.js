29d0e8b8df8fa8d609da243512226a62c70aadd0
KinhKhaVN <pebin0123@gmail.com>
2025-12-25 21:56:59 +0700

    Apply hover, active for button

---
+++ Button/index.js
@@ -0,0 +1,12 @@
+
+function changeClickMeButtonText()
+{
+        const buttonElement = document.getElementsByClassName('active-me');
+
+        buttonElement[0].textContent = "Meo Meo";
+        buttonElement[0].style.color = "orange";
+        buttonElement[0].style.backgroundColor = "lightblue";
+}
+
+
+
